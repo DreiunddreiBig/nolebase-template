90ab06ab9b46654d2f903d355610feb9deca9df1
pyzein <xxbeautiful@icloud.com>
2024-08-29 12:37:25 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -33,7 +33,7 @@ import {
   NolebaseUnlazyImg,
 } from '@nolebase/vitepress-plugin-thumbnail-hash/client'
 
-import { creators } from '../creators'
+
 
 import AppContainer from './components/AppContainer.vue'
 import DocFooter from './components/DocFooter.vue'
@@ -102,7 +102,7 @@ const ExtendedTheme: Theme = {
     })
 
     app.provide(NolebaseGitChangelogInjectionKey, {
-
+      mapContributors: creators,
     })
 
     app.use(NolebaseInlineLinkPreviewPlugin)
